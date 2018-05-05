10a9ed5bf36a81330239534e17edf871ba75315e
Taboo U Admin <support@taboou.com>
2018-05-05 06:59:02 -0400

    Basic actions in MobX.

--- src/store.js
+++ src/store.js
@@ -2,6 +2,9 @@ import { autorun, observable } from 'mobx'
 
 
 class TabooStore {
+	/* Initialize system variables. */
+	@observable hasAgreedToDisclaimer = false
+
 	@observable posts = ['come by tonight', 'new provider']
 	@observable filter = ''
 
@@ -25,6 +28,21 @@ class TabooStore {
 			completed : false,
             assignee  : null
 		})
+	}
+
+	/**
+	 * @notice Posters must agree to the service disclaimer. At the moment
+	 *		   all that is required is a button click. However, content 
+	 *         control in development & testing include:
+	 *             1. Phone number verification
+	 *             2. ID verification
+	 *             3. Other 3rd-party service verification
+	 */
+	agree() {
+console.log('i agree to eerything u say', this.hasAgreedToDisclaimer)
+		
+		/* Toggle agreed for boolean. */
+		this.hasAgreedToDisclaimer = true
 	}
 }
 
--- src/views/Post.js
+++ src/views/Post.js
@@ -12,36 +12,55 @@ export default class Post extends React.Component {
     }
 
     render() {
+        /* Retrieve the boolean for disclaimer agreement. */
+        const { hasAgreedToDisclaimer } = this.store
+
+        /* Process display. */
+        if (hasAgreedToDisclaimer)
+            return this.newPost()
+        else
+            return this.disclaimer()
+    }
+
+    agree() {
+console.log('fired the Post agree')
+        this.store.agree()
+    }
+
+    disclaimer() {
+        return (
+            <div class="disclaimerAgreement">
+                <h3>Disclaimer</h3>
+                
+                <div class="disclaimer-scroll-box">
+                    <p>
+                        This section contains sexual content, including pictorial nudity and adult language. 
+                        It is to be accessed only by persons who are 18 years of age or older (and is not considered to be a minor in his/her state of residence) and who live in a community or local jurisdiction where nude pictures and explicit adult materials are not prohibited by law. 
+                        By accessing this website, you are representing to us that you meet the above qualifications. 
+                        A false representation may be a criminal offense.
+                    </p>
+
+                    <p>
+                        I confirm and represent that I am 18 years of age or older (and am not considered to be a minor in my state of residence) and that I am not located in a community or local jurisdiction where nude pictures or explicit adult materials are prohibited by any law. 
+                        I agree to report any illegal services or activities which violate the Terms of Use. 
+                        I also agree to <a href="javascript://">report suspected exploitation of minors and/or human trafficking</a> to the appropriate authorities.
+                    </p>
+                </div>
+                
+                <p><b>I have read and agree to this disclaimer as well as the <a href="javascript://">Terms of Use</a>.</b></p>
+                <p><a href="javascript://" onClick={ this.agree.bind(this) }>I agree</a></p>
+                <p><a href="javascript://">I don't agree</a></p>
+            </div>
+        )
+    }
+
+    newPost() {
         return (
             <div>
+                We are good to go now folks!
+
                 <h2>NEW POST</h2>
-
-<h2>Post #1 { this.store.posts[0] }</h2>
-
-
-                <div class="disclaimerAgreement">
-                    <h3>Disclaimer</h3>
-                    
-                    <div class="disclaimer-scroll-box">
-                        <p>
-                            This section contains sexual content, including pictorial nudity and adult language. 
-                            It is to be accessed only by persons who are 18 years of age or older (and is not considered to be a minor in his/her state of residence) and who live in a community or local jurisdiction where nude pictures and explicit adult materials are not prohibited by law. 
-                            By accessing this website, you are representing to us that you meet the above qualifications. 
-                            A false representation may be a criminal offense.
-                        </p>
-
-                        <p>
-                            I confirm and represent that I am 18 years of age or older (and am not considered to be a minor in my state of residence) and that I am not located in a community or local jurisdiction where nude pictures or explicit adult materials are prohibited by any law. 
-                            I agree to report any illegal services or activities which violate the Terms of Use. 
-                            I also agree to <a href="javascript://">report suspected exploitation of minors and/or human trafficking</a> to the appropriate authorities.
-                        </p>
-                    </div>
-                    
-                    <p><b>I have read and agree to this disclaimer as well as the <a href="javascript://">Terms of Use</a>.</b></p>
-                    <p><a href="javascript://">I agree</a></p>
-                    <p><a href="javascript://">I don't agree</a></p>
-                </div>
-            </div>
+            </div>            
         )
     }
 }
